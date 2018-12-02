ec3de3426b74c07ac7e74505e53de46b1fb63fac
Débora Bianca <deborabiancatm@gmail.com>
2018-12-02 01:52:28 -0400

    site

---
+++ Site/script.js
@@ -0,0 +1,19 @@
+$.getJSON("https://api.myjson.com/bins/loqcy", { get_param: 'value' }, function(data) {
+    $.each(data, function(index, element) {
+        if(element.s1 >= 2 && element.s2 >= 2){
+            $('<img>').attr( "src", "img/vazio.jpg").appendTo( ".main").addClass('shelf');
+            $('.main').append($('<div>', {
+                text: '['+ element.ID +'] Prateleira de '+ element.tipo + ' está vazia.'}
+            ).addClass('emptyshelf'));
+            $('.lateral').append($('<div>', {
+                text: element.ID}).addClass('empty'));
+        } else {
+            $('<img>').attr( "src", "img/full.jpg" ).appendTo( ".main").addClass('shelf');
+            $('.main').append($('<div>', {
+                text: '['+ element.ID +'] Prateleira de '+ element.tipo + ' ainda possui produtos'}
+            ).addClass('fullshelf'));
+            $('.lateral').append($('<div>', {
+                text: element.ID }).addClass('full'));
+        }
+    });
+});
